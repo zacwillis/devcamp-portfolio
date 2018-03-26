bcb9606e452d6a2c4dabce1cac099ccb9fecd28b
Zac Willis <zwillis@relevantinc.com>
2018-03-26 16:57:26 -0600

    created gem and a lot other stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = WillisViewTool::Renderer.copyright "Zac Willis", "All rights reserved"
+  end
 end

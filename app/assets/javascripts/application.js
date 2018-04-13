4ff7b6a4511b678204c6b5abba266733c9524003
Zac Willis <zwillis@relevantinc.com>
2018-04-13 11:10:17 -0600

    Fixed JS loading bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
@@ -20,3 +20,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets

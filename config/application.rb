84d2d2b5cd03d102dd0a93caaa8288682936d076
Zac Willis <zwillis@relevantinc.com>
2018-04-13 10:14:56 -0600

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module DevcampPortfolio
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end

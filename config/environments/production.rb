029e38732ca5c152fb8077e3bbbaa1eaec37ee64
Zac Willis <zwillis@relevantinc.com>
2018-04-13 11:19:32 -0600

    Integrated produciton calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_cable.allowed_request_origins = ['https://zw-devcamp-portfolio.herokuapp.com', 'http://zw-devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://zw-devcamp-portfolio.herokuapp.com/cable"
 end
